bc1016a0e551f82897fdedcfab59648f0a12e763
jebishabariya <jebi312@gmail.com>
2025-01-05 12:54:47 +0545

    socket backend

--- backend/server.js
+++ backend/server.js
@@ -2,10 +2,12 @@
 import express from "express";
 import dotenv from "dotenv";
 import path from "path";
+import cookieParser from "cookie-parser";
 
 import { connectDB } from "./config/db.js";
 
 import productRoutes from "./routes/product.route.js";
+import messageRoutes from "./routes/message.routes.js";
 
 dotenv.config();
 
@@ -15,8 +17,11 @@ const PORT = process.env.PORT || 5000;
 const __dirname = path.resolve();
 
 app.use(express.json()); // allows us to accept JSON data in the req.body
+app.use(cookieParser());
+
 
 app.use("/api/products", productRoutes);
+app.use("/api/messages", messageRoutes);
 
 if (process.env.NODE_ENV === "production") {
 	app.use(express.static(path.join(__dirname, "/frontend/dist")));
