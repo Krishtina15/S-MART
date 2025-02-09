e9c2d4e23560d3b53dc8c61711978814a11b6eff
jebishabariya <jebi312@gmail.com>
2025-02-09 17:04:24 +0545

    files cleanup

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,3 @@
-
 import express from "express";
 import dotenv from "dotenv";
 import path from "path";
@@ -6,10 +5,9 @@ import cookieParser from "cookie-parser";
 import cors from "cors";
 
 import { connectDB } from "./config/db.js";
-import Product from "./models/product.model.js";
+//import Product from "./models/product.model.js";
 import authRoutes from "./routes/auth.routes.js";
 import productRoutes from "./routes/product.route.js";
-import messageRoutes from "./routes/message.routes.js";
 import offerRoutes from "./routes/offer.route.js";
 import profileRoutes from "./routes/profile.route.js";
 import notificationRoutes from "./routes/message.routes.js"
@@ -27,10 +25,9 @@ app.use('/uploads', express.static('uploads'));
 
 app.use("/api/auth", authRoutes);
 app.use("/api/products", productRoutes);
-app.use("/api/messages", messageRoutes);
 app.use("/api",offerRoutes);
 app.use("/api/user",profileRoutes);
-app.use("/api",notificationRoutes);
+app.use("/api", notificationRoutes);
 
 if (process.env.NODE_ENV === "production") {
 	app.use(express.static(path.join(__dirname, "/frontend")));
